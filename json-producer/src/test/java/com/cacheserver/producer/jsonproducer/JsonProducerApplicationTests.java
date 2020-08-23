c2f9b0154e027560f859d978c9fe580a641c06cf
Edris <eeffathi@yahoo.com>
2020-08-24 02:13:45 +0430

    setting up maven and libraries

---
+++ json-producer/src/test/java/com/cacheserver/producer/jsonproducer/JsonProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacheserver.producer.jsonproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
